4b3ebedd75f4bb05e59046d1b7f9257d1e06dfc4
BoornKolb <tivokb@gmail.com>
2013-10-28 08:36:16 -0700

    Create Pythontest.py

---
+++ Pythontest.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+now = datetime.now() 
+print now
+print now.year
+print now.month
+print now.day
+print str(now.month)+"/" + str(now.day)+"/" + str(now.year)
+print str(now.hour)+":" + str(now.minute)+":"+str(now.second)
+print str(now.month)+"/" + str(now.day)+"/" + str(now.year) + " " +str(now.hour)+":" + str(now.minute)+":"+str(now.second)
