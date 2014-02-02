06dfdb8a16766c49947c1d8ea07063fe5d9d7030
Hobson Lane <hobsonlane@gmail.com>
2014-02-02 10:29:55 -0800

    add eb ignore

---
+++ application.py
@@ -0,0 +1,11 @@
+import flask
+
+application = flask.Flask(__name__)
+application.debug=True
+ 
+@application.route('/')
+def hello_world():
+    return "Hello world!"
+ 
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', debug=True)
